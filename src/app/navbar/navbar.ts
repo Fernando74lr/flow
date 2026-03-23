a5c8ee51c6f9a6f60d41593d49eb6863436a730a
Fernando López Ramírez <flopezramirez@hotmail.com>
2026-03-22 18:01:36 -0600

    feat(theme): change theme option

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -9,4 +9,11 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  isDarkTheme = signal(true);
+
+  toggleTheme() {
+    this.isDarkTheme.update((value) => !value);
+    document.body.classList.toggle('light-theme', !this.isDarkTheme());
+  }
+}
